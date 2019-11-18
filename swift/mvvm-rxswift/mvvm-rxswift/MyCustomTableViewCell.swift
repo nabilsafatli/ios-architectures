360b4e696cddc090908c349da512295a619447d0
nabil.safatli <safatli.nabil@gmail.com>
2019-11-18 10:17:56 +0100

    Create simple table view in mvvm-rx project

---
+++ swift/mvvm-rxswift/mvvm-rxswift/MyCustomTableViewCell.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class MyCustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- swift/mvvm-rxswift/mvvm-rxswift/ViewController.swift
+++ swift/mvvm-rxswift/mvvm-rxswift/ViewController.swift
@@ -1,12 +1,27 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(MyCustomTableViewCell.self, forCellReuseIdentifier: "MyCustomTableViewCell")
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCustomTableViewCell", for: indexPath) as? MyCustomTableViewCell else {
+            preconditionFailure("\(MyCustomTableViewCell.self) not found")
+        }
+
+        cell.labelTitle?.text = "Nabil"
+
+        return cell
+    }
 
 }
 
