513ff067c7064a8a57a81ab3e5e266ee0e12f412
nabil.safatli <safatli.nabil@gmail.com>
2019-11-23 18:24:10 +0100

    Add resources of images to display characters and create CharacterService class

---
+++ swift/mvvm-rxswift/mvvm-rxswift/CharactersService.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+protocol CharacterServiceProtocol {
+    func getCharacters(with completion:([Character]) -> Void)
+}
+
+final class CharactersService: CharacterServiceProtocol {
+    func getCharacters(with completion: ([Character]) -> Void) {
+        let characters = [Character]()
+        completion(characters)
+    }
+}
+
+struct Character {
+    var name: String
+}
--- swift/mvvm-rxswift/mvvm-rxswift/ViewController.swift
+++ swift/mvvm-rxswift/mvvm-rxswift/ViewController.swift
@@ -5,20 +5,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.register(MyCustomTableViewCell.self, forCellReuseIdentifier: "MyCustomTableViewCell")
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 5
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCustomTableViewCell", for: indexPath) as? MyCustomTableViewCell else {
             preconditionFailure("\(MyCustomTableViewCell.self) not found")
         }
-
-        cell.labelTitle?.text = "Nabil"
+        cell.profileImageView?.image = UIImage(named: "archer")
+        cell.labelTitle?.text = "Archer"
 
         return cell
     }
